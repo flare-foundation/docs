f8760cee54a2b6a228d69505967f1cf7e0183628
Xavi Artigas <xavierartigas@yahoo.es>
2022-10-04 10:45:49 +0200

    Add a title to all images that allow zoom
    So that on hover you get a "Click to enlarge" message.

--- docs/assets/javascripts/images.js
+++ docs/assets/javascripts/images.js
@@ -2,6 +2,7 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; ++i) {
     const im = images[i];
     if (im.classList.contains("allow-zoom")) {
+        im.title = 'Click to enlarge';
         im.outerHTML = '<a target="_blank" href="' + im.src + '">' + im.outerHTML + '</a>';
     }
 };
