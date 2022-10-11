e59bf3822ddf52fab8a7abda814fc5241f9704ea
Xavi Artigas <xavierartigas@yahoo.es>
2022-10-11 10:44:52 +0200

    Make all external links open in a new tab
    No more need for manually adding {target=_blank} to MD links.

---
+++ docs/assets/javascripts/links.js
@@ -0,0 +1,8 @@
+var links = document.getElementsByTagName('a');
+for (var i = 0, l = links.length; i < l; ++i) {
+    const lk = links[i];
+    if (lk.hostname !== location.hostname) {
+        lk.setAttribute("target", "_blank");
+        lk.setAttribute("rel", "noopener noreferrer");
+    }
+};
