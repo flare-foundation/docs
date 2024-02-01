f3dd73c5f4607a4dac9a1e6b88f4dec5355938bb
Swarnabha Sinha <swarnabhasinha.201mn059@nitk.edu.in>
2024-02-01 12:07:24 +0100

    add: State Connector tutorial WIP

---
+++ samples/sc/contracts/addressValidityVerification.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "@flarenetwork/state-connector-protocol/contracts/generated/verification/AddressValidityVerification.sol";
